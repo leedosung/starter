f8421e31a95520a5b2430b2979ecee4d7ac16054
leedosung <dosungl@chekt.com>
2025-01-14 12:19:26 +0900

    feat(mappings): add key mapping for json and yaml format

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,6 +8,14 @@ local map = vim.keymap.set
 --
 -- map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("n", "<Leader>jq", "<cmd>%!jq<cr>", { desc = "Format JSON" })
-map("n", "<Leader>jc", "<cmd>%!jq -c<cr>", { desc = "Format JSON (compact)" })
+map("n", "<Leader>jf", "<cmd>%!jq<cr>", { desc = "Format JSON" })
+map("n", "<Leader>jc", "<cmd>%!jq -c<cr>", { desc = "Minify JSON" })
+
+map("n", "<Leader>xf", "<cmd>%!xmllint --format -<cr>", { desc = "Format XML" })
+map("n", "<Leader>xc", "<cmd>%!xmllint --noblanks -<cr>", { desc = "Minify XML" })
+
 map("n", "<Leader>fr", "<cmd>Telescope resume<cr>", { desc = "Telescope resume" })
+
+map("n", "<Leader>da", "<cmd>windo diffthis<cr>", { desc = "Buffer all windows diff" })
+map("n", "<Leader>do", "<cmd>windo diffoff<cr>", { desc = "Buffer all windows diff off" })
+map("n", "<Leader>du", "<cmd>windo diffupdate<cr>", { desc = "Buffer all windows diff update" })
